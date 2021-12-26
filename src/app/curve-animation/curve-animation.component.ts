4f9b87cf84ce28e31bab612c4af490271f45e604
Skwal98 <levkalex98@gmail.com>
2021-12-26 19:37:26 +0300

    refactoring curve

--- src/app/curve-animation/curve-animation.component.ts
+++ src/app/curve-animation/curve-animation.component.ts
@@ -1,16 +1,18 @@
 import {
   animate,
   AnimationBuilder,
+  AnimationStyleMetadata,
   keyframes,
   style,
 } from '@angular/animations';
 import {
+  ChangeDetectorRef,
   Component,
   ElementRef,
   HostListener,
   Input,
-  OnInit,
-  ViewChild,
+  QueryList,
+  ViewChildren,
 } from '@angular/core';
 
 export const easing = {
@@ -22,69 +24,105 @@ export const easing = {
   templateUrl: './curve-animation.component.html',
   styleUrls: ['./curve-animation.component.scss'],
 })
-export class CurveAnimationComponent implements OnInit {
+export class CurveAnimationComponent {
   @Input() public target: string = 'RU';
   @Input() public source: string = 'EN';
 
-  @ViewChild('c') el?: ElementRef;
-  @ViewChild('e') el2?: ElementRef;
+  @ViewChildren('block') blocks?: QueryList<ElementRef>;
+
+  private _isAnimation = false;
 
   constructor(
     private builder: AnimationBuilder,
-    private _element: ElementRef
+    private _element: ElementRef,
+    private _cd: ChangeDetectorRef
   ) {}
 
   @HostListener('click') onClick() {
-    this.start(this.el!, 0, false);
-    this.start(this.el2!, 0, true);
-    console.log('cl');
+    if (!this._isAnimation) {
+      this._isAnimation = true;
+      this._startAnimation();
+    }
+  }
+
+  public get sourceBlock(): ElementRef {
+    return this.blocks!.first;
+  }
+
+  public get targetBlock(): ElementRef {
+    return this.blocks!.last;
   }
 
   public get width(): number {
     return (
       this._element.nativeElement.children[0].getBoundingClientRect().width -
-      this.el!.nativeElement.getBoundingClientRect().width
+      this.targetBlock!.nativeElement.getBoundingClientRect().width
     );
   }
 
-  ngOnInit(): void {}
+  private _startAnimation() {
+    this.blocks!.forEach((x, i) => {
+      const data = this._calculateTransform(i === 1);
 
-  start(el: ElementRef, rDiff: number, fl: boolean) {
-    const r = this.width / 2;
-    const stepsNumber = 180;
-    const steps = Array(stepsNumber)
-      .fill(null)
-      .map((e, i) => {
-        const degree = i - rDiff;
-        let x = r - r * Math.cos((degree * Math.PI) / 180);
-        let y = r * Math.sin((degree * Math.PI) / 180);
-        if (fl) {
-          x = x * -1;
+      const animationPlayer = this.builder
+        .build(animate(`300ms ${easing.standard}`, keyframes(data)))
+        .create(x!.nativeElement);
+
+      animationPlayer.play();
+      animationPlayer.onDone(() => {
+        animationPlayer.destroy();
+        if (i === 0) {
+          this._moveItems();
+          this._cd.detectChanges();
+          this._isAnimation = false;
         }
-        if (!fl) {
-          y = y * -1;
-        }
-        const transform = style({
-          transform: `translate(${x}px, ${y}px)`,
-        });
-        return transform;
       });
-
-    console.log(steps);
-
-    const animationPlayer = this.builder
-      .build(animate(`300ms ${easing.standard}`, keyframes(steps)))
-      .create(el!.nativeElement);
-    animationPlayer.play();
-    animationPlayer.onDone(() => {
-      if (el == this.el) {
-        this._animationEnd();
-      }
-      animationPlayer.destroy();
     });
   }
 
-  private _animationEnd() {
+  private _backupAnimationStyle: AnimationStyleMetadata[][] = [];
+
+  private _calculateTransform(up: boolean): AnimationStyleMetadata[] {
+    let backup = this._getFromBackup(up);
+    if (backup !== null) {
+      return backup;
+    }
+
+    const stepsNumber = 180;
+    const r = this.width / 2;
+    const data = Array(stepsNumber)
+      .fill(null)
+      .map((e, i) => {
+        const degree = i;
+        let x = r - r * Math.cos((degree * Math.PI) / 180);
+        let y = r * Math.sin((degree * Math.PI) / 180);
+        if (up) {
+          x = x * -1;
+        } else {
+          y = y * -1;
+        }
+        return style({
+          transform: `translate(${x}px, ${y}px)`,
+        });
+      });
+    this._saveToBackup(up, data);
+    return data;
+  }
+
+  private _getFromBackup(up: boolean): AnimationStyleMetadata[] | null {
+    const index = up ? 0 : 1;
+    if (this._backupAnimationStyle[index] !== undefined) {
+      return this._backupAnimationStyle[index];
+    }
+    return null;
+  }
+
+  private _saveToBackup(up: boolean, data: AnimationStyleMetadata[]): void {
+    const index = up ? 0 : 1;
+    this._backupAnimationStyle[index] = data;
+  }
+
+  private _moveItems() {
     const temp = this.target;
     this.target = this.source;
     this.source = temp;
